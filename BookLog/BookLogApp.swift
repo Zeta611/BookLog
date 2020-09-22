6797b496e79615907fdbfea9715f58889ea9e243
Jaeho Lee <jaeho.lee@snu.ac.kr>
2020-09-22 15:19:26 +0900

    :tada: Initial commit

---
+++ BookLog/BookLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLogApp.swift
+//  BookLog
+//
+//  Created by Jay Lee on 2020/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BookLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
